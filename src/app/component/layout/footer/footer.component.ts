669725e642b813f2c7fcbba88063c5a38af23d0d
Abrew <abrewabraham@gmail.com>
2023-09-10 17:18:51 +0530

    res

--- src/app/component/layout/footer/footer.component.ts
+++ src/app/component/layout/footer/footer.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent
+{
+
+  @Output() customEvent: EventEmitter<string> = new EventEmitter();
+
+  // To trigger the event in the child component
+
+  sectionReval(event: string): void
+  {
+    this.customEvent.emit(event);
+  }
 
 }
